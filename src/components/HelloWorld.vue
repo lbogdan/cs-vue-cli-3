4cceb2f466f054ffc89a2b2c48e62b56d7beeda3
Bogdan Luca <luca.bogdan@gmail.com>
2019-03-22 20:57:16 +0200

    Remove uneeded modules.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="hello">
     <h1 v-html="msg"/>
-    <h2>{{ cuid() }}</h2>
     <p>
       For guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -32,15 +31,10 @@
 </template>
 
 <script>
-import cuid from 'cuid';
-
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
-  },
-  methods: {
-    cuid
   }
 }
 </script>
